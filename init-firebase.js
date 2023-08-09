3f7911287aa3c35f91e07cb1883f1d707b35618e
Bunjee Awoyomi <aoa26@cornell.edu>
2023-08-09 11:57:52 -0400

    first commit

--- init-firebase.js
+++ init-firebase.js
@@ -3,7 +3,12 @@
  * provided for you by firebase after creating your project!
 */
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyBpcM8aK5ejQwCchJuswAbx1o9IDiojomI",
+    authDomain: "firemessages-e719b.firebaseapp.com",
+    projectId: "firemessages-e719b",
+    storageBucket: "firemessages-e719b.appspot.com",
+    messagingSenderId: "1098630051704",
+    appId: "1:1098630051704:web:9c5877d85c039618347a8d"
 };
 
 // initialize the firebase app with this built-in function
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /**
  * @TODO get a reference to the Firebase Database object
  */
-
+const database = firebase.database().ref();
 /**
  * @TODO get const references to the following elements:
  *      - div with id #all-messages
@@ -10,6 +10,11 @@
  *      - button with id #send-btn and the updateDB
  *        function as an onclick event handler
  */
+const allMessages = document.getElementById('all-messages');
+const usernameElem = document.getElementById('username');
+const messageElem = document.getElementById('message');
+const sendBtn = document.getElementById('send-btn');
+sendBtn.onclick = updateDB;
 
 /**
  * @TODO create a function called updateDB which takes
@@ -21,13 +26,29 @@
  *      - writes this object to the database
  *      - resets the value of #message input element
  */
+function updateDB(event){
+    //stop the form from refreshing
+    event.preventDefault();
+
+    //store the data into a temp object
+    const data = {
+        USERNAME: usernameElem.value,
+        MESSAGE: messageElem.value
+    };
+
+    //writing to the database
+    database.push(data);
+
+    //reset the value of message element
+    messageElem.value = "";
+}
 
 /**
  * @TODO add the addMessageToBoard function as an event
  * handler for the "child_added" event on the database
  * object
  */
-
+database.on("child_added", addMessageToBoard);
 /**
  * @TODO create a function called addMessageToBoard that
  * takes one parameter rowData which:
@@ -38,6 +59,16 @@
  *        #all-messages (we should have a reference already!)
  * 
  */
+function addMessageToBoard(rowData){
+    //get the object passed from the firebase
+    const data = rowData.val();
+    console.log(data);
+
+    //get the singleMessage using the makeSingleMessageHTML function
+    let singleMessage = makeSingleMessageHTML(data.USERNAME, data.MESSAGE);
+
+    allMessages.append(singleMessage);
+}
 
 /** 
  * @TODO create a function called makeSingleMessageHTML which takes
@@ -56,6 +87,30 @@
  * 
  *      - returns the parent div
  */
+function makeSingleMessageHTML(usernameTxt, messageTxt){
+    //create a parentDiv
+    let parentDiv = document.createElement("div");
+    //adding a class 'single-message' to parentDiv
+    parentDiv.classList.add('single-message');
+
+    //creating a p element
+    let usernameP = document.createElement("p");
+    //adding a class 'single-message-username'
+    usernameP.classList.add('single-message-username');
+    //displaying the username in the p element
+    usernameP.innerHTML = usernameTxt + ":";
+    //append usernameP to div
+    parentDiv.append(usernameP);
+
+    //create another p element
+    let messageP = document.createElement("p");
+    //updates innerHTML to have the message
+    messageP.innerHTML = messageTxt;
+    //append messageP to div
+    parentDiv.append(messageP);
+
+    return parentDiv;
+}
 
 /**
  * @BONUS add an onkeyup event handler to the form HTML
